1fbe4f164e84fe85050c391bb5ccfcd8e78d987c
axener2 <axener2@gmail.com>
2025-09-09 23:34:29 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -13,3 +13,16 @@
     });
   }
 })();
+
+
+// Mobile menu toggle (robust)
+(function(){
+  const toggle = document.querySelector('.menu-toggle');
+  const nav = document.getElementById('site-nav');
+  if (toggle && nav){
+    toggle.addEventListener('click', function(){
+      const open = nav.classList.toggle('open');
+      this.setAttribute('aria-expanded', open ? 'true' : 'false');
+    });
+  }
+})();
